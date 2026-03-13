34885ea5429564b1345bcd3c84a48c18fa802043
Lars Karlitski <lars@karlitski.net>
2026-03-13 17:10:43 +0000

    Edit: Print operation as word

--- library/Edit.js
+++ library/Edit.js
@@ -52,7 +52,8 @@ export default class Edit {
   }
 
   toString() {
-    return this.#args.path;
+    let op = this.#args.find ? "Edit" : "Create";
+    return `${op} ${this.#args.path}`;
   }
 
   get symbol() {
